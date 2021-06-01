fdd1e7df5193ea84ff6bcd2a2d1e407db610535e
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-01 09:31:42 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,40 @@
+import re
+import requests
+import argparse
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import utils
+
+
+home_url = "https://www.transfermarkt.com"
+
+LEAGUE_TABLES = {
+	"premier": "Premier League",
+	"laliga": "LaLiga",
+	"serie": "Serie A",
+	"bundesliga": "Bundesliga"
+}
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Process leauge top scorers')
+	parser.add_argument('--league_link_name', default='premier', help='the name to use to get the league link')
+	args = parser.parse_args()
+	soup = utils.get_soup(home_url)
+
+	league_link = home_url + utils.get_league_link(soup, args.league_link_name)
+	league_soup = utils.get_soup(league_link)
+
+	league_table = utils.get_league_table(league_soup)
+
+	stats_url = home_url + league_soup.find('a', title="View stats")['href']
+	stats_soup = utils.get_soup(stats_url)
+	stats_uris = utils.get_stats_uris(stats_soup)
+
+	players_stats_urls = utils.get_players_stats_urls(stats_uris)
+
+	df = utils.get_scorers_df(players_stats_urls, LEAGUE_TABLES[args.league_link_name], league_table)
+
+	df.fillna(0, inplace=True)
+	df.to_csv("data/{}_scorers.csv".format(args.league_link_name), index=False)
