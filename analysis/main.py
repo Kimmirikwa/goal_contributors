e6f7b5a84f2a580fa79e45b7a76a597127052a5e
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-03 05:20:47 +0300

    add minutes played

--- analysis/main.py
+++ analysis/main.py
@@ -9,5 +9,8 @@ if __name__ == "__main__":
 	# goal_contibution = goals + assists
 	data['goal_contribution'] = data.apply(lambda row: row['goals'] + row['assists'], axis=1)
 
+	# minutes as int
+	data['minutes_played'] = data.apply(lambda row: int(row['minutes_played'][:-1]), axis=1)
+
 	top_half = utils.get_top_half_goal_contributions(data)
 	print(top_half.head(10))
