4c4b2d5beb2954820743dc02bc5215da05e87ecc
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-03 06:09:22 +0300

    round values

--- analysis/main.py
+++ analysis/main.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
 	data.fillna(0, inplace=True)
 
 	# goal_contibution = goals + assists
-	data['goal_contribution'] = data.apply(lambda row: row['goals'] + row['assists'], axis=1)
-	print(data.head())
+	data['goal_contribution'] = data['goals'] + data['assists']
 
 	# minutes as int
 	data['minutes_played'] = data.apply(lambda row: int(row['minutes_played'][:-1]), axis=1)
 
-	top_half = utils.get_top_half_goal_contributions(data)
-	print(top_half.head(10))
+	contibutors_against_top_10_teams = utils.aggregate_goal_contributions(
+		data[data['opponent_position'] <= 10])
+	contibutors_against_top_10_teams.to_csv("output/contibutors_against_top_10_teams.csv", index=False)
--- analysis/utils.py
+++ analysis/utils.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 
-def get_top_half_goal_contributions(df):
+def aggregate_goal_contributions(df):
 	"""
-	get goal contibutions against teams that finished in the top half
+	aggregate goal contibutors
 	"""
-	top_half_df = df[df['opponent_position'] <= 10]
-	top_half_df = top_half_df[['player', 'goal_contribution', 'goals', 'assists', 'minutes_played']]
+	df = df[['player', 'goal_contribution', 'goals', 'assists', 'minutes_played']]
 
-	aggregates = top_half_df.groupby(['player']).agg('sum').sort_values(
+	aggregates = df.groupby(['player']).agg('sum').sort_values(
 		by=['goal_contribution', 'goals', 'assists'], ascending=False)
-	aggregates['minutes_per_goal_contibution'] = aggregates.apply(lambda row: row['minutes_played'] / row['goal_contribution'], axis=1)
+	aggregates['minutes_per_goal_contibution'] = aggregates['minutes_played'] / aggregates['goal_contribution']
+	aggregates = aggregates.round(1)
 
-	return aggregates
+	return aggregates.reset_index()
