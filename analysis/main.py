39d28f2c81294a5152b0449a07abf83b0e91a84a
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-03 07:13:22 +0300

    add games played and use goal_involvement

--- analysis/main.py
+++ analysis/main.py
@@ -8,11 +8,30 @@ if __name__ == "__main__":
 	data.fillna(0, inplace=True)
 
 	# goal_contibution = goals + assists
-	data['goal_contribution'] = data['goals'] + data['assists']
+	data['goal_involvement(goals + assists)'] = data['goals'] + data['assists']
 
 	# minutes as int
 	data['minutes_played'] = data.apply(lambda row: int(row['minutes_played'][:-1]), axis=1)
 
-	contibutors_against_top_10_teams = utils.aggregate_goal_contributions(
+	# add games played column
+	# the values should be 1 but will be summed per player to get the total count
+	data['games_played'] = 1
+
+	# overall scorers and assiter aggregates
+	goal_contibutor_aggregates = utils.aggregate_goal_involvements(data)
+	goal_contibutor_aggregates.to_csv("output/goal_contibutor_aggregates.csv", index=False)
+
+	# scorers or assisters agains top 10 teams
+	contributors_against_top_10_teams = utils.aggregate_goal_involvements(
 		data[data['opponent_position'] <= 10])
-	contibutors_against_top_10_teams.to_csv("output/contibutors_against_top_10_teams.csv", index=False)
+	contributors_against_top_10_teams.to_csv("output/contributors_against_top_10_teams.csv", index=False)
+
+	# scorers or assisters against bottom 10 teams
+	contributors_against_bottom_10_teams = utils.aggregate_goal_involvements(
+		data[data['opponent_position'] >= 11])
+	contributors_against_bottom_10_teams.to_csv("output/contributors_against_bottom_10_teams.csv", index=False)
+
+	# scorers or assisters agains top 4 teams
+	contributors_against_top_4_teams = utils.aggregate_goal_involvements(
+		data[data['opponent_position'] <= 4])
+	contributors_against_top_4_teams.to_csv("output/contributors_against_top_4_teams.csv", index=False)
--- analysis/utils.py
+++ analysis/utils.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
 
-def aggregate_goal_contributions(df):
+def aggregate_goal_involvements(df):
 	"""
 	aggregate goal contibutors
 	"""
-	df = df[['player', 'goal_contribution', 'goals', 'assists', 'minutes_played']]
+	df = df[['player', 'goal_involvement(goals + assists)', 'goals', 'assists', 'minutes_played', 'games_played']]
 
 	aggregates = df.groupby(['player']).agg('sum').sort_values(
-		by=['goal_contribution', 'goals', 'assists'], ascending=False)
-	aggregates['minutes_per_goal_contibution'] = aggregates['minutes_played'] / aggregates['goal_contribution']
+		by=['goal_involvement(goals + assists)', 'goals', 'assists'], ascending=False)
+	# avoid division by zero
+	# we are also only intrested in players that scored or assisted
+	aggregates = aggregates[aggregates['goal_involvement(goals + assists)'] > 0]
+	aggregates['minutes_per_goal_involvement'] = aggregates['minutes_played'] / aggregates['goal_involvement(goals + assists)']
+	aggregates['games_per_goal_involvement'] = aggregates['games_played'] / aggregates['goal_involvement(goals + assists)']
 	aggregates = aggregates.round(1)
+	aggregates.reset_index(inplace=True)
 
-	return aggregates.reset_index()
+	return aggregates
