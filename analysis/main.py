f35d4a7cdeec1438bc21b109f0d9e7bcd8b56ebc
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-03 05:43:52 +0300

    correct bundesliga data and some analysis

--- analysis/main.py
+++ analysis/main.py
@@ -5,9 +5,11 @@ import utils
 
 if __name__ == "__main__":
 	data = pd.read_csv("../data/premier_scorers.csv")
+	data.fillna(0, inplace=True)
 
 	# goal_contibution = goals + assists
 	data['goal_contribution'] = data.apply(lambda row: row['goals'] + row['assists'], axis=1)
+	print(data.head())
 
 	# minutes as int
 	data['minutes_played'] = data.apply(lambda row: int(row['minutes_played'][:-1]), axis=1)
--- analysis/utils.py
+++ analysis/utils.py
@@ -2,8 +2,14 @@ import pandas as pd
 
 
 def get_top_half_goal_contributions(df):
+	"""
+	get goal contibutions against teams that finished in the top half
+	"""
 	top_half_df = df[df['opponent_position'] <= 10]
-	top_half_df = top_half_df[['player', 'goal_contribution', 'goals', 'assists']]
+	top_half_df = top_half_df[['player', 'goal_contribution', 'goals', 'assists', 'minutes_played']]
 
-	return top_half_df.groupby(['player']).agg('sum').sort_values(
+	aggregates = top_half_df.groupby(['player']).agg('sum').sort_values(
 		by=['goal_contribution', 'goals', 'assists'], ascending=False)
+	aggregates['minutes_per_goal_contibution'] = aggregates.apply(lambda row: row['minutes_played'] / row['goal_contribution'], axis=1)
+
+	return aggregates
