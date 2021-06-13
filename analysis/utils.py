64df0e8e87e496adc56a03a2d7b0c3ba65303bd6
kimmirikwa <kimrodrikwa@gmail.com>
2021-06-13 12:21:46 +0300

    add goals by venue

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
 
+VENUE_GOALS = {
+	'A': 'away_goals',
+	'H': 'home_goals'
+}
+
 
 def aggregate_goal_involvements(df):
 	"""
 	aggregate goal contibutors
 	"""
-	df = df[['player', 'goal_involvement(goals + assists)', 'goals', 'assists', 'minutes_played', 'games_played']]
+	df_to_aggregate = df[['player', 'goal_involvement(goals + assists)', 'goals', 'assists', 'minutes_played', 'games_played']]
 
-	aggregates = df.groupby(['player']).agg('sum').sort_values(
+	aggregates = df_to_aggregate.groupby(['player']).agg('sum').sort_values(
 		by=['goal_involvement(goals + assists)', 'goals', 'assists'], ascending=False)
 	# avoid division by zero
 	# we are also only intrested in players that scored or assisted
@@ -20,7 +25,18 @@ def aggregate_goal_involvements(df):
 	aggregates = aggregates.round(1)
 	aggregates.reset_index(inplace=True)
 
+	venue_goals_aggregates = get_goals_by_venue(df[['player', 'venue', 'goals']])
+
+	aggregates = pd.merge(aggregates, venue_goals_aggregates, left_on='player', right_on='player')
+
 	return aggregates
+
+
+def get_goals_by_venue(df_for_venues):
+	df_for_venues['venue'] = df_for_venues.apply(lambda row: VENUE_GOALS[row['venue']], axis=1)
+	venue_goals_aggregates = pd.pivot_table(
+		df_for_venues, index=['player'], columns=['venue'], values='goals', aggfunc=np.sum)
+	return venue_goals_aggregates
 
 
 def aggregate_goals_types(df):
